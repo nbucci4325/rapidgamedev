541da871c09fb66d806d28a400170f9aaec53ea3
onlao25 <adrianscheng25@gmail.com>
2026-02-06 20:13:27 -0500

    2/6/26 - 8:11PM - PROGRAMMING: Created _weaponManager script allowing for weapon swaps
    Created script _weaponManager, which allows for weapon swaps.

---
+++ RapidGameDev/Assets/Programming/Scripts/Managers/_weaponManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class _weaponManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform[] weapons;
+
+    [Header("Keys")]
+    [SerializeField] private KeyCode[] keys;
+
+    [Header("Settings")]
+    [SerializeField] private float switchTime;
+
+    private int selectedWeapon;
+    private float timeSinceLastSwitch;
+
+    private void Start()
+    {
+        SetWeapons();
+        Select(selectedWeapon);
+
+        timeSinceLastSwitch = 0f;
+    }
+
+    private void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        for(int i = 0; i < keys.Length; i++)
+        {
+            if (Input.GetKeyDown(keys[i]))
+            {
+                selectedWeapon = i;
+                OnWeaponSelected();
+            }
+        }
+
+        if (previousSelectedWeapon != selectedWeapon) Select(selectedWeapon);
+
+        timeSinceLastSwitch = Time.deltaTime;
+    }
+
+    private void SetWeapons()
+    {
+        weapons = new Transform[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            weapons[i] = transform.GetChild(i);
+        }
+
+        print("Weapon Count: " + weapons.Length);
+
+        if (keys == null) keys = new KeyCode[weapons.Length];
+    }
+
+    private void Select(int weaponIndex)
+    {
+        for(int i = 0; i < weapons.Length; i++)
+        {
+            weapons[i].gameObject.SetActive(i == weaponIndex);
+        }
+
+        timeSinceLastSwitch = 0f;
+
+        OnWeaponSelected();
+    }
+
+    private void OnWeaponSelected()
+    {
+        print("Selected new weapon.");
+    }
+}
