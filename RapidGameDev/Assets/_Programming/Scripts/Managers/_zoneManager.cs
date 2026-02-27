67e1d44a74d9f3b5200d7f660c58738998c4c954
nbucci4325 <buccillinico@gmail.com>
2026-02-27 12:07:11 -0500

    02/27/26 - 12:06PM - PROGRAMMING: Compile issue fixed... again

--- RapidGameDev/Assets/_Programming/Scripts/Managers/_zoneManager.cs
+++ RapidGameDev/Assets/_Programming/Scripts/Managers/_zoneManager.cs
@@ -3,63 +3,63 @@ using UnityEngine.Rendering;
 
 public class zoneControl : MonoBehaviour
 {
-    [SerializeField] GameObject[] spawners;
-    [SerializeField] int[] zoneQuotas;
-    private int zoneQuota = 0;
+    //[SerializeField] GameObject[] spawners;
+    //[SerializeField] int[] zoneQuotas;
+    //private int zoneQuota = 0;
 
-    private void Start()
-    {
-        foreach (GameObject spawner in spawners) spawner.SetActive(false);
-        zoneQuota = zoneQuotas[0];
-    }
+    //private void Start()
+    //{
+    //    foreach (GameObject spawner in spawners) spawner.SetActive(false);
+    //    zoneQuota = zoneQuotas[0];
+    //}
 
     //private void Update()
     //{
 
     //}
 
-    //    [SerializeField] private int zoneQuota = 0;
-    //    [SerializeField] private GameObject[] spawners;
-    //    [SerializeField] private bool firstZoneInLevel = false;
+    [SerializeField] private int zoneQuota = 0;
+    [SerializeField] private GameObject[] spawners;
+    [SerializeField] private bool firstZoneInLevel = false;
 
-    //    private void Start()
-    //    {
-    //        if (!firstZoneInLevel) foreach (GameObject spawner in spawners) spawner.SetActive(false);
-    //    }
+    private void Start()
+    {
+        if (!firstZoneInLevel) foreach (GameObject spawner in spawners) spawner.SetActive(false);
+    }
 
-    //    private void Update()
-    //    {
-    //        if (zoneQuota <= 0)
-    //        {
-    //            Debug.Log("ZONE COMPLETE");
-    //            zoneComplete();
-    //        }
-    //    }
+    private void Update()
+    {
+        if (zoneQuota <= 0)
+        {
+            Debug.Log("ZONE COMPLETE");
+            zoneComplete();
+        }
+    }
 
-    //    private void OnTriggerEnter(Collider other)
-    //    {
-    //        if (other.CompareTag("Player"))
-    //        {
-    //            gameObject.GetComponent<Collider>().enabled = false;
-    //            //play animation of door opening here
-    //            foreach (GameObject spawner in spawners)
-    //            {
-    //                spawner.SetActive(true);
-    //            }
-    //        }
-    //    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.GetComponent<Collider>().enabled = false;
+            //play animation of door opening here
+            foreach (GameObject spawner in spawners)
+            {
+                spawner.SetActive(true);
+            }
+        }
+    }
 
-    //    private void zoneComplete()
-    //    {
-    //        foreach (GameObject spawner in spawners)
-    //        {
-    //            spawner.SetActive(false);
-    //        }
-    //    }
+    private void zoneComplete()
+    {
+        foreach (GameObject spawner in spawners)
+        {
+            spawner.SetActive(false);
+        }
+    }
 
-    //    public void decrementZoneQuota()
-    //    {
-    //        zoneQuota--;
-    //        Debug.Log("QUOTA DECREMENTED");
-    //    }
+    public void decrementZoneQuota()
+    {
+        zoneQuota--;
+        Debug.Log("QUOTA DECREMENTED");
+    }
 }
