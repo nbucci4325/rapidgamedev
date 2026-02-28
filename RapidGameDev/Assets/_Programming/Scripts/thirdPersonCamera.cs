3ad2a8a0832c1a773b358052535f6aa5d0ac210c
onlao25 <adrianscheng25@gmail.com>
2026-02-28 12:35:42 -0500

    02/28/26 - 12:35PM - PROGRAMMING: Tweaked Level 1 Camera
    Made some tweaks to Level 1 camera. Still needs to be perfected, will return to.

--- RapidGameDev/Assets/_Programming/Scripts/thirdPersonCamera.cs
+++ RapidGameDev/Assets/_Programming/Scripts/thirdPersonCamera.cs
@@ -31,10 +31,10 @@ public class thirdPersonCamera : MonoBehaviour
     private void Update()
     {
         //rotate orientation
-        //transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        //orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
-        orientation.forward = viewDir.normalized;
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        //Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        //orientation.forward = viewDir.normalized;
 
         Vector3 dirToLookAt = lookHere.position - new Vector3(transform.position.x, lookHere.position.y, transform.position.z);
         orientation.forward = dirToLookAt.normalized;
@@ -43,7 +43,7 @@ public class thirdPersonCamera : MonoBehaviour
 
 
         //get mouse input
-        //float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        //float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
     }
 }
